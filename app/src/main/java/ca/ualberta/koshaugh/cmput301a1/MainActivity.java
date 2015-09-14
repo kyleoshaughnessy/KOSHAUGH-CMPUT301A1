f3732c2fe66b7ceedac094e00625b9e207b4f11e
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2015-09-14 15:50:16 -0600

    Added information dialog for the Reaction timer mode.

--- app/src/main/java/ca/ualberta/koshaugh/cmput301a1/MainActivity.java
+++ app/src/main/java/ca/ualberta/koshaugh/cmput301a1/MainActivity.java
@@ -1,5 +1,7 @@
 package ca.ualberta.koshaugh.cmput301a1;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -40,8 +42,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void onReactionTimerButtonClick(View v) {
         Toast.makeText(this,"Enter Reaction Timer Mode", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, ReactionTimerActivity.class);
-        startActivity(intent);
+        final Intent intent = new Intent(this, ReactionTimerActivity.class);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("This is the Reaction Timer Mode!\n" +
+                "To begin, press OK and then start the countdown, after the countdown has " +
+                "finished wait for the alert message before pressing the reaction button.");
+        builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                startActivity(intent);
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     public void onGameshowButtonClick(View v) {
