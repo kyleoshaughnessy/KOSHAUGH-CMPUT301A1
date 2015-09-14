b1deb6fe640f445d9055e9124a9655de3bb7aafe
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2015-09-14 15:16:32 -0600

    Created Layouts and an activity for the game show button variations. Also added an activity for the Reaction Timer mode.

--- app/src/main/java/ca/ualberta/koshaugh/cmput301a1/gameshowModeActivity.java
+++ app/src/main/java/ca/ualberta/koshaugh/cmput301a1/gameshowModeActivity.java
@@ -1,25 +1,22 @@
 package ca.ualberta.koshaugh.cmput301a1;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class GameshowModeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game_show_2_players);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_gameshow_mode, menu);
         return true;
     }
 
@@ -36,17 +33,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void onReactionTimerButtonClick(View v) {
-        Toast.makeText(this,"Enter Reaction Timer Mode", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, ReactionTimerActivity.class);
-        startActivity(intent);
-    }
-
-    public void onGameshowButtonClick(View v) {
-        Toast.makeText(this,"Enter Gameshow Button Mode", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, GameshowModeActivity.class);
-        startActivity(intent);
     }
 }
