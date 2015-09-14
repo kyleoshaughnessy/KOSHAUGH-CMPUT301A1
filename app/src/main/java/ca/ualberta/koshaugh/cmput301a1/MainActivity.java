b70fc1bf71ce65f01bd28b08a94e650c412a5fb6
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2015-09-14 08:29:31 -0600

    Added onClick events to the main activity, onClick will produce coresponding debug Toast messages

--- app/src/main/java/ca/ualberta/koshaugh/cmput301a1/MainActivity.java
+++ app/src/main/java/ca/ualberta/koshaugh/cmput301a1/MainActivity.java
@@ -1,9 +1,12 @@
 package ca.ualberta.koshaugh.cmput301a1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +36,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onReactionTimerButtonClick(View v) {
+        Toast.makeText(this,"Enter Reaction Timer Mode", Toast.LENGTH_SHORT).show();
+//        Intent intent = new Intent(this, ReactionTimerActivity.class);
+    }
+
+    public void onGameshowButtonClick(View v) {
+        Toast.makeText(this,"Enter Gameshow Button Mode", Toast.LENGTH_SHORT).show();
+//        Intent intent = new Intent(this, GameshowButtonActivity.class);
     }
 }
