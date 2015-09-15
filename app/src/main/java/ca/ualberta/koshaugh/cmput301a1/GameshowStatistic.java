74cbdea2f11031daf1bed4d936a66dd108396154
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2015-09-15 11:11:02 -0600

    Added classes for statistics of both user modes

---
+++ app/src/main/java/ca/ualberta/koshaugh/cmput301a1/GameshowStatistic.java
@@ -0,0 +1,41 @@
+package ca.ualberta.koshaugh.cmput301a1;
+
+/**
+ * Created by kyleoshaughnessy on 15-09-15.
+ */
+public class GameshowStatistic {
+    public enum Player {
+        PLAYER_ONE("Player One"),
+        PLAYER_TWO("Player Two"),
+        PLAYER_THREE("Player Three"),
+        PLAYER_FOUR("Player Four");
+
+        private String id;
+
+        Player(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+    }
+
+    private Player winningPlayer;
+
+    public GameshowStatistic(Player winningPlayer) {
+        this.winningPlayer = winningPlayer;
+    }
+
+    public Player getWinningPlayer() {
+        return winningPlayer;
+    }
+
+    public void setWinningPlayer(Player winningPlayer) {
+        this.winningPlayer = winningPlayer;
+    }
+}
---
+++ app/src/main/java/ca/ualberta/koshaugh/cmput301a1/ReactionStatistic.java
@@ -0,0 +1,43 @@
+package ca.ualberta.koshaugh.cmput301a1;
+
+import java.util.Date;
+
+/**
+ * Created by kyleoshaughnessy on 15-09-15.
+ */
+public class ReactionStatistic {
+    private Date startTime;
+    private Date endTime;
+
+    public ReactionStatistic() {
+    }
+
+    public ReactionStatistic(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public ReactionStatistic(Date startTime, Date endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Long getReactionTime() {
+        return endTime.getTime() - startTime.getTime();
+    }
+}
