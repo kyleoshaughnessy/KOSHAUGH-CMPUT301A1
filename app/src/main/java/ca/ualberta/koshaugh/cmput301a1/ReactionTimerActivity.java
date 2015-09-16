f9f528b7d610e00106c71107fe0f16c15c981474
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2015-09-15 19:06:45 -0600

    finished 'countdown' timer which then initiates the reaction button countdown sequence

--- app/src/main/java/ca/ualberta/koshaugh/cmput301a1/ReactionTimerActivity.java
+++ app/src/main/java/ca/ualberta/koshaugh/cmput301a1/ReactionTimerActivity.java
@@ -1,16 +1,21 @@
 package ca.ualberta.koshaugh.cmput301a1;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class ReactionTimerActivity extends AppCompatActivity {
 
-    private static final Handler taskSchedular = new Handler();
+    private static final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +46,21 @@ public class ReactionTimerActivity extends AppCompatActivity {
     }
 
     public void onStartCountdownButtonClick(View view) {
-        Toast.makeText(this, "Countdown Started!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "On your marks!", Toast.LENGTH_SHORT).show();
+        final Drawable reactionButtonBackground =
+                findViewById(R.id.reactionButton).getBackground();
 
-        final ReactionTimerActivity temp = this;
+        reactionButtonBackground.setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
 
         Runnable task = new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(temp, "Delayed Message", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ReactionTimerActivity.this, "Get set!", Toast.LENGTH_SHORT).show();
+                reactionButtonBackground.setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP);
             }
         };
 
-        taskSchedular.postDelayed(task, 5000);
+        handler.postDelayed(task, 3000);
+
     }
 }
