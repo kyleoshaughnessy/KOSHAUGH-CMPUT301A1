2d44381234d894a2c5422292df764aea92a8245d
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2015-09-26 22:10:14 -0600

    fully removed options menu from main activity. linked stats activity to the options menu in gameshowmode

--- app/src/main/java/ca/ualberta/koshaugh/cmput301a1/gameshowModeActivity.java
+++ app/src/main/java/ca/ualberta/koshaugh/cmput301a1/gameshowModeActivity.java
@@ -1,6 +1,7 @@
 package ca.ualberta.koshaugh.cmput301a1;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -37,21 +38,6 @@ public class GameshowModeActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void onPlayerButtonClick(View view) {
         GameshowStatistic stat = null;
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -77,7 +63,10 @@ public class GameshowModeActivity extends AppCompatActivity {
             GameshowStatisticManager.getManager().addStatistic(stat);
             builder.show();
         }
+    }
 
-
+    public void onStatisticsMenuClick(MenuItem menu) {
+        Intent intent = new Intent(this, GameshowStatisticActivity.class);
+        startActivity(intent);
     }
 }
